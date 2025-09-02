2eecf8212c9b559fcffe43370d7626a4ea3aa9d2
Filip Sakač <filip.sakac@uprise.hr>
2025-09-03 01:14:15 +0200

    Statut example

--- src/pages/Constitution/Constitution.tsx
+++ src/pages/Constitution/Constitution.tsx
@@ -4,112 +4,36 @@ import "./Constitution.css";
 const Constitution = () => {
     return (
         <FancyContainer>
-            {" "}
+            <h3>I. OPĆE ODREDBE</h3>
+            <h4>Čanak 1.</h4>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis justo in
-                pharetra efficitur. Vivamus pulvinar dapibus accumsan. Integer tempus, ex fermentum
-                ultricies rhoncus, erat lacus lacinia justo, nec commodo augue odio ac urna. Nunc
-                feugiat sollicitudin semper. Aliquam sit amet porta massa, quis ullamcorper justo.
-                Nullam mauris est, euismod et cursus id, porta eu ipsum. Quisque suscipit commodo
-                venenatis. Donec eleifend ultricies lectus, vitae ultrices orci convallis sed. Nam
-                quis metus tempus, tincidunt orci a, fermentum orci. Phasellus sagittis diam at
-                nulla condimentum semper. Quisque gravida leo eu sem consectetur, sed malesuada leo
-                lobortis. Quisque commodo nisi eu ligula accumsan ullamcorper.
+                Klub ljubitelja Pive & Knjige (u daljnjem tekstu: Klub) osnovan je kao udruga osoba
+                koje dijele zajedničku strast prema čitanju knjiga i uživanju u pivi, ne nužno tim
+                redoslijedom. Klub djeluje na temelju načela slobodne volje, međusobnog uvažavanja i
+                zajedničkog sudjelovanja u aktivnostima.
             </p>
+            <h4>Članak 2.</h4>
             <p>
-                Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed non hendrerit
-                neque, a feugiat erat. Vivamus non sapien sodales tortor dignissim rutrum.
-                Pellentesque posuere condimentum odio, sit amet gravida ante euismod non. Vestibulum
-                posuere felis sit amet fringilla faucibus. Donec quis nulla sit amet velit hendrerit
-                rutrum. In hac habitasse platea dictumst. Maecenas sed nisi non augue porttitor
-                bibendum a sed quam. Sed tempor posuere tincidunt. Maecenas faucibus ut lectus sit
-                amet auctor. Cras molestie erat nec dapibus mollis.
+                Sjedište Kluba i adresa sastajanja određuje se odlukom osnivača i može se mijenjati
+                isključivo odlukom predviđenom ovim Statutom.
             </p>
+
+            <h3>II. OSNIVAČI I UPRAVLJANJE KLUBOM</h3>
+            <h4>Članak 3.</h4>
             <p>
-                Quisque dictum, dolor in efficitur molestie, odio nunc sodales orci, eget tincidunt
-                erat quam et enim. Quisque viverra, urna vitae ultricies rutrum, felis magna
-                accumsan metus, vel venenatis lectus nisi mattis lorem. Integer ipsum lectus,
-                tincidunt nec cursus hendrerit, consectetur non ligula. Vestibulum at finibus massa.
-                Suspendisse pharetra arcu sed erat mattis, quis dapibus velit vulputate. Quisque
-                sagittis hendrerit fermentum. Praesent eleifend porta neque et facilisis. Vivamus
-                vitae tellus et nisi luctus vulputate sit amet ut justo. Pellentesque a mi sed neque
-                sodales auctor. Donec eu aliquam lectus. Proin vel mauris convallis, consectetur
-                velit blandit, gravida dolor. Maecenas vehicula sem ac imperdiet maximus. Nullam
-                ornare nec odio sed ultricies. Morbi nibh lectus, eleifend et ultricies eget, ornare
-                et enim.
+                Klub su osnovala četiri člana (u daljnjem tekstu: Osnivači), koji imaju apsolutnu
+                upravlja ku moć nad Klubom. Osnivači su:
             </p>
+            <ul>
+                <li>Irena Kinder</li>
+                <li>Josip Nimac </li>
+                <li>Filip Sakač</li>
+                <li>Filip Škoda</li>
+            </ul>
+            <h4>Članak 4.</h4>
             <p>
-                Donec hendrerit augue dictum arcu dapibus porttitor. Praesent mattis venenatis nisi,
-                quis faucibus felis ornare scelerisque. Integer vitae leo ac magna bibendum lacinia
-                non eget nisi. Sed vestibulum fringilla varius. Quisque ut rutrum nibh, quis dapibus
-                ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-                ridiculus mus. Donec interdum sem non posuere sagittis. In condimentum, lorem eget
-                porttitor venenatis, libero lorem luctus magna, vel vehicula nibh purus sed dui.
-                Cras venenatis fringilla nibh, a condimentum turpis tempus non. Donec blandit libero
-                at enim vehicula tincidunt.
-            </p>
-            <p>
-                Aliquam lacinia ipsum a auctor bibendum. Aliquam quis purus nunc. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla
-                varius quis sapien sit amet ultrices. In molestie dui sed neque vestibulum, vitae
-                tincidunt libero elementum. In hac habitasse platea dictumst. Cras eget neque quis
-                libero hendrerit tincidunt sed at magna. Suspendisse eget varius diam, nec mattis
-                ipsum. Duis sagittis sem pharetra blandit aliquet. Maecenas maximus dui in nisl
-                placerat, non lacinia eros pharetra. Aenean efficitur pellentesque metus, in auctor
-                augue iaculis et. Cras id placerat massa, sed placerat quam.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis justo in
-                pharetra efficitur. Vivamus pulvinar dapibus accumsan. Integer tempus, ex fermentum
-                ultricies rhoncus, erat lacus lacinia justo, nec commodo augue odio ac urna. Nunc
-                feugiat sollicitudin semper. Aliquam sit amet porta massa, quis ullamcorper justo.
-                Nullam mauris est, euismod et cursus id, porta eu ipsum. Quisque suscipit commodo
-                venenatis. Donec eleifend ultricies lectus, vitae ultrices orci convallis sed. Nam
-                quis metus tempus, tincidunt orci a, fermentum orci. Phasellus sagittis diam at
-                nulla condimentum semper. Quisque gravida leo eu sem consectetur, sed malesuada leo
-                lobortis. Quisque commodo nisi eu ligula accumsan ullamcorper.
-            </p>
-            <p>
-                Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed non hendrerit
-                neque, a feugiat erat. Vivamus non sapien sodales tortor dignissim rutrum.
-                Pellentesque posuere condimentum odio, sit amet gravida ante euismod non. Vestibulum
-                posuere felis sit amet fringilla faucibus. Donec quis nulla sit amet velit hendrerit
-                rutrum. In hac habitasse platea dictumst. Maecenas sed nisi non augue porttitor
-                bibendum a sed quam. Sed tempor posuere tincidunt. Maecenas faucibus ut lectus sit
-                amet auctor. Cras molestie erat nec dapibus mollis.
-            </p>
-            <p>
-                Quisque dictum, dolor in efficitur molestie, odio nunc sodales orci, eget tincidunt
-                erat quam et enim. Quisque viverra, urna vitae ultricies rutrum, felis magna
-                accumsan metus, vel venenatis lectus nisi mattis lorem. Integer ipsum lectus,
-                tincidunt nec cursus hendrerit, consectetur non ligula. Vestibulum at finibus massa.
-                Suspendisse pharetra arcu sed erat mattis, quis dapibus velit vulputate. Quisque
-                sagittis hendrerit fermentum. Praesent eleifend porta neque et facilisis. Vivamus
-                vitae tellus et nisi luctus vulputate sit amet ut justo. Pellentesque a mi sed neque
-                sodales auctor. Donec eu aliquam lectus. Proin vel mauris convallis, consectetur
-                velit blandit, gravida dolor. Maecenas vehicula sem ac imperdiet maximus. Nullam
-                ornare nec odio sed ultricies. Morbi nibh lectus, eleifend et ultricies eget, ornare
-                et enim.
-            </p>
-            <p>
-                Donec hendrerit augue dictum arcu dapibus porttitor. Praesent mattis venenatis nisi,
-                quis faucibus felis ornare scelerisque. Integer vitae leo ac magna bibendum lacinia
-                non eget nisi. Sed vestibulum fringilla varius. Quisque ut rutrum nibh, quis dapibus
-                ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-                ridiculus mus. Donec interdum sem non posuere sagittis. In condimentum, lorem eget
-                porttitor venenatis, libero lorem luctus magna, vel vehicula nibh purus sed dui.
-                Cras venenatis fringilla nibh, a condimentum turpis tempus non. Donec blandit libero
-                at enim vehicula tincidunt.
-            </p>
-            <p>
-                Aliquam lacinia ipsum a auctor bibendum. Aliquam quis purus nunc. Orci varius
-                natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla
-                varius quis sapien sit amet ultrices. In molestie dui sed neque vestibulum, vitae
-                tincidunt libero elementum. In hac habitasse platea dictumst. Cras eget neque quis
-                libero hendrerit tincidunt sed at magna. Suspendisse eget varius diam, nec mattis
-                ipsum. Duis sagittis sem pharetra blandit aliquet. Maecenas maximus dui in nisl
-                placerat, non lacinia eros pharetra. Aenean efficitur pellentesque metus, in auctor
-                augue iaculis et. Cras id placerat massa, sed placerat quam.
+                Sva ključna pitanja i odluke vezane za djelovanje Kluba, uključujući promjene ovog
+                Statuta, donose se jednoglasno od strane svih Osnivača.
             </p>
         </FancyContainer>
     );
