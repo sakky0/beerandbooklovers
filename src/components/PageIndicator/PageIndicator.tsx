50811a47eda7419619cf444d2881e4755a4a21f9
Filip Sakač <filip.sakac@uprise.hr>
2025-09-03 10:23:01 +0200

    Remove page indicator icon

--- src/components/PageIndicator/PageIndicator.tsx
+++ src/components/PageIndicator/PageIndicator.tsx
@@ -1,5 +1,4 @@
 import { useLocation } from "react-router-dom";
-import { navgationItems } from "../../helpers/navigationHelper";
 import "./PageIndicator.css";
 
 const pageTitles = new Map();
@@ -11,18 +10,13 @@ const getTitle = (location: string) => {
     return pageTitles.has(location) ? pageTitles.get(location) : "Literatura";
 };
 
-const getIcon = (location: string) => {
-    let navItem = navgationItems.filter(i => i.link === location)?.[0] ?? navgationItems[0];
-    return navItem.icon;
-};
 
 const PageIndicator = () => {
-    let location = useLocation();
+    const location = useLocation();
 
     return (
         <div className="page-indicator">
             <h2>{getTitle(location.pathname)}</h2>
-            {getIcon(location.pathname)}
         </div>
     );
 };
