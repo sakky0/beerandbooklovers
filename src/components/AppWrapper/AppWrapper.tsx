c37f5a2666c27134f0d9ee1623ee6a61248808db
Filip Sakač <filip.sakac@uprise.hr>
2025-09-09 22:25:26 +0200

    Removed toast

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -1,8 +1,6 @@
 import { HashRouter as Router } from "react-router-dom";
-import { Toast } from "radix-ui";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import "./AppWrapper.css";
 
 export type IAppWrapper = React.PropsWithChildren;
 
@@ -17,12 +15,7 @@ const queryClient = new QueryClient({
 const AppWrapper = ({ children }: IAppWrapper) => {
     return (
         <QueryClientProvider client={queryClient}>
-            <Router>
-                <Toast.Provider>
-                    <Toast.Viewport id="toast-viewport" />
-                    {children}
-                </Toast.Provider>
-            </Router>
+            <Router>{children}</Router>
 
             <ReactQueryDevtools buttonPosition="bottom-right" />
         </QueryClientProvider>
