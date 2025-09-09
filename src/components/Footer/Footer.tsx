9016dc211f833eb1cfab62eaa41db31acda9f2a6
Filip Sakač <filip.sakac@uprise.hr>
2025-09-09 22:10:52 +0200

    Added site version

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,15 @@
+import { version } from "../../../package.json";
 import "./Footer.css";
 
 const year = new Date().getFullYear();
 
 const Footer = () => {
-    return <footer>&copy; {year} Klub ljubitelja pive i knjige - Uprise</footer>;
+    return (
+        <footer>
+            <span>&copy; {year} Klub ljubitelja pive i knjige - Uprise</span>
+            <span className="version">v{version}</span>
+        </footer>
+    );
 };
 
 export default Footer;
